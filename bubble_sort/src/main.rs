24063338bf750d7f401afa2296be082789d16875
gccMango <gcc.mango@gmail.com>
2022-10-15 12:31:53 +0900

    Add Bubble sort

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -1,3 +1,23 @@
+fn bubble_sort(vec: &mut Vec<i32>) {
+    let mut sorted_flag = false;
+    let mut unsorted_index = vec.len() - 1;
+    while !sorted_flag {
+        sorted_flag = true;
+        for index in 0..unsorted_index {
+            if vec[index] > vec[index + 1] {
+                vec.swap(index, index + 1);
+                sorted_flag = false;
+            }
+        }
+        unsorted_index-=1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec = vec![-1, 3, -55, 30, -42, 55, -13];
+    println!("vec 0st sorted {:?}", vec);
+
+    bubble_sort(&mut vec);
+
+    println!("vec sorted {:?}", vec);
 }
